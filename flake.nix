6f0516ef684d691fcdada520bb0ba71f82a2d943
verziet <garlic-boss-batch@duck.com>
2025-05-16 11:34:31 +0200

    feat: add git hooks and an  script

--- flake.nix
+++ flake.nix
@@ -2,17 +2,15 @@
   description = "Description for the project";
 
   inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    git-hooks-nix.url = "github:cachix/git-hooks.nix";
   };
 
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
       imports = [
-        # To import a flake module
-        # 1. Add foo to inputs
-        # 2. Add foo as a parameter to the outputs function
-        # 3. Add here: foo.flakeModule
+        inputs.git-hooks-nix.flakeModule
       ];
       systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
       perSystem = {
@@ -23,13 +21,13 @@
         system,
         ...
       }: {
-        # Per-system attributes can be defined here. The self' and inputs'
-        # module parameters provide easy access to attributes of the same
-        # system.
+        pre-commit.settings.hooks = {
+          alejandra.enable = true;
+        };
 
-        # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
-        packages.default = pkgs.hello;
         formatter = pkgs.alejandra;
+
+        packages.install-git-hooks = pkgs.writeShellScriptBin "install-git-hooks" config.pre-commit.installationScript;
       };
       flake = {
         # The usual flake attributes can be defined here, including system-
