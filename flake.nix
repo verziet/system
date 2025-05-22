353e658f11c12ee1de9a218b8f04fd3c0e2a3d38
verziet <garlic-boss-batch@duck.com>
2025-05-22 18:25:18 +0000

    build: add nixpkgs stable input

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "Description for the project";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs?ref=nixos-24.11";
 
     flake-parts.url = "github:hercules-ci/flake-parts";
     git-hooks-nix.url = "github:cachix/git-hooks.nix";
