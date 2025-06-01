50f2946f2424f90fef379518793bd597c0129995
verziet <garlic-boss-batch@duck.com>
2025-06-01 20:55:49 +0000

    chore: idk at this point x2

--- modules/nixos/desktop/gnome/default.nix
+++ modules/nixos/desktop/gnome/default.nix
@@ -8,7 +8,7 @@ in {
   config = lib.mkIf config.${moduleName}.enableModule {
     services = {
       desktopManager.gnome.enable = true;
-      desktopManager.gdm.enable = true;
+      displayManager.gdm.enable = true;
     };
   };
 
